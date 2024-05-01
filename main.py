c5a63ba36fb20566837491713e0d82284a36df5e
Ryan Galloway <ryan.galloway@shaw.ca>
2024-05-01 11:39:30 -0600

    Prepare for web deployment

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -27,7 +27,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -37,7 +37,7 @@ login_manager = LoginManager()
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 login_manager.init_app(app)
